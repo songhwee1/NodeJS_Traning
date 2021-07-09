bad6b833837d837fc2a23762457d456eaf713991
songhwee1 <songhwee1@naver.com>
2021-07-09 09:38:05 +0900

    add ejs module, sever with index.html

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const app = express();
 const server = app.listen(3000, () => {
     console.log('Start Server : localhost:3000');
 });
+
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+app.engine('html',require('ejs').renderFile);
+
+app.get('/', function(req,res) {
+    res.render('index.html')
+})
+
+app.get('/about', function(req,res) {
+    res.send('about page')
+})
