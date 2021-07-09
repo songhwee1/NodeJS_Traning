666a2d0c4f1e406a354380b312b7736cf4b5b2ef
songhwee1 <songhwee1@naver.com>
2021-07-09 09:55:58 +0900

    complete tutorial - db data to JSON

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const server = app.listen(3000, () => {
     console.log('Start Server : localhost:3000');
 });
 
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : '127.0.0.1',
+    user            : 'root',
+    password        : '1234',
+    database        : 'bbs'
+});
+
 app.set('views', __dirname + "/views");
 app.set('view engine', 'ejs');
 app.engine('html',require('ejs').renderFile);
@@ -16,3 +25,18 @@ app.get('/', function(req,res) {
 app.get('/about', function(req,res) {
     res.send('about page')
 })
+
+
+app.get('/db',function(req,res){
+    pool.getConnection(function(err,connection){
+        if (err) throw err;
+
+        connection.query('select * from bbs', function (error,results, fields){
+            res.send(JSON.stringify(results));
+            console.log('results', results);
+            connection.release();
+
+            if (error) throw error;
+        });
+    });
+});
