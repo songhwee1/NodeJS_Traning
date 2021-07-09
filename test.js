0f73aa661720340f9c7aea6fa42f30460924d110
songhwee1 <songhwee1@naver.com>
2021-07-09 09:19:22 +0900

    send email example with nodemailer SMTP

--- test.js
+++ test.js
@@ -1,7 +1,31 @@
-console.log("Hello");
+const nodemailer = require('nodemailer');
+const email = {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "eb17aa40e64063",
+      pass: "dca5fc162d22dc"
+    }
+};
 
-let a = 2;
-let b = 3;
-let c = a+b;
+const send =  async ( option ) => {
+    nodemailer.createTransport(email).sendMail(option,(error,info) => {
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log(info);
+            return info.response;
+        }
 
-console.log("c: "+c);
+    })  
+};
+
+let emailData = {
+    from: 'songhwee1@naver.com',
+    to: 'songhwee1@naver.com',
+    subject: '메일 테스트',
+    text: '나는 코딩천재'
+}
+
+send(emailData);
